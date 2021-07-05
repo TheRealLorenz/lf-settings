83ab268e03a0ba5ff6b135796fc61670e610809d
Lollo <lollo@laptop.localdomain>
2021-07-05 19:19:40 +0200

    Add lf install from yay

--- lf-settings.sh
+++ lf-settings.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir ~/.config/lf
+mkdir -pv ~/.config/lf
 
 cp ./{lfrc,preview,cleaner} ~/.config/lf/
+
+yay -S lf
 
 sudo pacman -S --overwrite '*' bc ueberzug ffmpegthumbnailer imagemagick poppler gnome-epub-thumbnailer chafa unzip p7zip unrar catdoc docx2txt odt2txt 
 
